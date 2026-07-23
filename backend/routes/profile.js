84d9c040f51a4bd179c221d4798cac54f3afd7e2
saurav528 <work.saurav22@gmail.com>
2026-07-23 16:50:35 +0530

    small change to routes/profile.js

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {updateProfile, deleteAccount, getUserDetails, getEnrolledCourses} = require("../controllers/Profile");
-const {auth, isStudent} = require("../middlewares/auth");
+const { updateProfile, deleteAccount, getUserDetails, getEnrolledCourses } = require("../controllers/Profile");
+const { auth, isStudent } = require("../middlewares/auth");
 
 router.put("/updateprofile", auth, updateProfile);  // tested
 router.delete("/deleteaccount", auth, deleteAccount); // tested
-router.get("/getuserdetails", auth, getAllUserDetails); // tested
+router.get("/getuserdetails", auth, getUserDetails); // tested
 router.get("/enrolledcourses", auth, isStudent, getEnrolledCourses); // tested
-
 
 module.exports = router;
